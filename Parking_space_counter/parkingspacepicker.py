d36f78cd1a7e5400c2f9a72626d51a09cf6f36bc
Tejaswi Kashyap <106374216+Tejaswi-kashyap-006@users.noreply.github.com>
2023-03-05 14:01:13 +0530

    Add files via upload

---
+++ Parking_space_counter/parkingspacepicker.py
@@ -0,0 +1,35 @@
+import cv2
+import pickle
+import cvzone
+import numpy as np
+
+img = cv2.imread('carParkImg.png')
+width, height = 103, 43
+try:
+    with open('CarParkPos', 'rb') as f:
+        posList = pickle.load(f)
+except:
+    posList = []
+
+
+def mouse_click(events, x, y, flags, params):
+    if events == cv2.EVENT_LBUTTONDOWN:
+        posList.append((x, y))
+    if events == cv2.EVENT_RBUTTONDOWN:
+        for i, pos in enumerate(posList):
+            x1, y1 = pos
+            if x1 < x < x1 + width and y1 < y < y1 + height:
+                posList.pop(i)
+
+    with open('CarParkPos', 'wb') as f:
+        pickle.dump(posList, f)
+
+
+while True:
+    img = cv2.imread('carParkImg.png')
+    for pos in posList:
+        cv2.rectangle(img, pos, (pos[0] + width, pos[1] + height), (255, 0, 255), 2)
+
+    cv2.imshow("Image", img)
+    cv2.setMouseCallback("Image", mouse_click)
+    cv2.waitKey(1)
